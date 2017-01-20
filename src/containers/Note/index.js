da9abd001e4bd7ae4eefbc35d81fbc112309bc72
ak1103dev <ak.apichan1103@gmail.com>
2017-01-20 12:08:19 +0700

    add save button

--- src/containers/Note/index.js
+++ src/containers/Note/index.js
@@ -1,29 +1,40 @@
 import React, { Component } from 'react';
 
 export default class Note extends Component {
+  onSubmit = () => {
+    const data = {
+      type: this.refs.type.value,
+      price: this.refs.price.value,
+      name: this.refs.name.value,
+      date: this.refs.date.value
+    }
+    console.log('submit', data)
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-md-12">
           <h1>Note</h1>
-          <form>
+          <div>
             <div className="form-group">
               <label>Type</label>
-              <input type="text" className="form-control"/>
+              <input ref="type" type="text" className="form-control"/>
             </div>
             <div className="form-group">
               <label>Price</label>
-              <input type="number" className="form-control" defaultValue={0} min={0} />
+              <input ref="price" type="number" className="form-control" defaultValue={0} min={0} />
             </div>
             <div className="form-group">
               <label>Name</label>
-              <input type="text" className="form-control"/>
+              <input ref="name" type="text" className="form-control"/>
             </div>
             <div className="form-group">
               <label>Date</label>
-              <input type="datetime-local" className="form-control"/>
+              <input ref="date" type="datetime-local" className="form-control"/>
             </div>
-          </form>
+            <button className="btn btn-primary" onClick={this.onSubmit}>Save</button>
+          </div>
         </div>
       </div>
     );
