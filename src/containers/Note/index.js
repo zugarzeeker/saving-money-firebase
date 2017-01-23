b9282e365967b5cb2967170588296d0685417192
Supanut Apikulvanich <supanut.overload@gmail.com>
2017-01-23 21:27:17 +0700

    Use react-datepicker

--- src/containers/Note/index.js
+++ src/containers/Note/index.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default class Note extends Component {
-  onSubmit = () => {
+  constructor() {
+    super();
+    this.state = {
+      startDate: moment()
+    };
+  }
+  onSubmit() {
     const data = {
       type: this.refs.type.value,
       price: this.refs.price.value,
       name: this.refs.name.value,
-      date: this.refs.date.value
+      // date: this.refs.date.value,
+      date: this.state.startDate,
+      time: this.refs.time.value
     }
     console.log('submit', data)
+  }
+
+  handleChange(newDate) {
+    this.setState({
+      startDate: newDate
+    });
   }
 
   render() {
@@ -30,10 +47,18 @@ export default class Note extends Component {
               <input ref="name" type="text" className="form-control"/>
             </div>
             <div className="form-group">
-              <label>Date</label>
-              <input ref="date" type="datetime-local" className="form-control"/>
+              <label>Date</label><br/>
+              <DatePicker
+                className="form-control"
+                selected={this.state.startDate}
+                onChange={this.handleChange.bind(this)}
+              />
             </div>
-            <button className="btn btn-primary" onClick={this.onSubmit}>Save</button>
+            <div className="form-group">
+              <label>Time</label>
+              <input ref="time" type="time" className="form-control"/>
+            </div>
+            <button className="btn btn-primary" onClick={this.onSubmit.bind(this)}>Save</button>
           </div>
         </div>
       </div>
